5c85c0761d07165412170e6fb603d24063dc40ae
jy <jiaying.jen@gmail.com>
2020-02-12 17:54:08 +0800

    Updated scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -33,11 +34,6 @@ class ScheduleScraper:
     @staticmethod
     def load_url(url: str):
 
-        # TODO: might need to tweak the methods
-        # logic == might need to navigate to the next week to scrape
-        # purge older records
-        # check the dates to make sure that the info being scraped == new
-
         """Loads a url and waits until schedule table elements appear.
 
         Arguments:
@@ -49,17 +45,12 @@ class ScheduleScraper:
         """
 
         browser = webdriver.Chrome(executable_path="reference_files/chromedriver.exe")
-
-        # TODO: add the wait condition > else page content is empty
-        # wait = WebDriverWait(driver, 10)
-        # element = wait.until(EC.)
-
         browser.get(url)
 
         return browser
 
     @staticmethod
-    def get_soup(browser: str):
+    def get_soup(browser):
 
         """Gets page source and returns a BeautifulSoup object.
 
@@ -70,13 +61,23 @@ class ScheduleScraper:
             soup {BeautifulSoup} -- Soup; parsed page source
         """
 
+        # TODO: add the wait condition > else page content is empty
+        # wait condition HAS to go here
+        # wait = WebDriverWait(driver, 10)
+        # element = wait.until(EC.)
+
         html = browser.page_source
         soup = BeautifulSoup(html, 'lxml')
 
         return soup
 
     @staticmethod
-    def parse_soup(soup: BeautifulSoup):
+    def parse_page(soup: BeautifulSoup,
+                   class_type: str):
+
+        # TODO: parsing doesn't work for multi entries
+        # i.e. if there's more than one class durig a certain time slot
+        # see: AST Fitness
 
         """Parses BeautifulSoup object to extract class schedule.
 
@@ -88,10 +89,96 @@ class ScheduleScraper:
                 extracted schedule
         """
 
-        tag_class_def = soup.find_all('div', {"class": "class class-default"})
         tag_has_class = soup.find_all('td', {"data-has-class": "1"})
+        df_list = []
 
-        return tag_class_def, tag_has_class
+        for tag in tag_has_class:
+            temp_df = pd.DataFrame()
+            date = tag['data-date']
+            time = tag['data-time']
+            get_text = lambda tag: [t.text for t in tag]
+            class_names = get_text(tag.find_all('span', {'class':'class-type'}))
+            teachers = get_text(tag.find_all('span', {'class':'class-teacher'}))
+            durations = get_text(tag.find_all('span', {'class':'duration'}))
+
+            temp_df = pd.DataFrame({'date': date,
+                                    'time': time,
+                                    'class': class_names,
+                                    'teacher': teachers,
+                                    'duration': durations})
+            df_list += [temp_df]
+
+        df = pd.concat(df_list)
+        df['class_type'] = class_type
+
+        return df
+
+    def get_next_week(self,
+                      browser):
+
+        """Navigates to next week's schedule.
+
+        Arguments:
+            browser {webdriver.chrome.webdriver.WebDriver} -- Browser object
+                to navigate in
+        Returns:
+            browser {webdriver.chrome.webdriver.WebDriver} -- Updated browser object
+        """
+        # TODO: THIS METHOD
+        # click button
+        # go to next week
+        # return browser object
+        browser = None
+
+        return browser
+
+    def scrape_all(self):
+
+        # TODO: test this
+
+        soup_bowl = []
+        for k,v in self.url_dict:
+            browser = self.load_url(v)
+            soup = self.get_soup(browser)
+            soup_bowl += [(k, soup)]
+
+            browser = self.get_next_week(browser)
+            soup = self.get_soup(browser)
+            soup_bowl += [(k, soup)]
+
+        parsed_soup = []
+        for class_type, soup in soup_bowl:
+            df = self.parse_page(soup=soup,
+                                 class_type=class_type)
+            parsed_soup += [df]
+
+        full_df = pd.concat(parsed_soup)
+
+        return full_df
+
+    @staticmethod
+    def convert_dt(df: pd.core.frame.DataFrame):
+
+        current_year = datetime.now().year
+        combined_dt = df.apply(lambda row: ('{} {} {}').format(
+            row['date'],
+            current_year,
+            row['time']),
+            axis = 1)
+        datetimes = pd.to_datetime(combined_dt,
+                                   format ='%a %b %d %Y %H:%M')
+        df['start_dt'] = datetimes
+        df['end_dt'] = df.apply(lambda row:
+            row['start_dt'] + timedelta(minutes=int(row['duration'])),
+            axis = 1)
+
+        df.sort_values(by = 'start_dt',
+                       ascending = True,
+                       inplace = True)
+
+        reordered_df = df[['start_dt', 'end_dt', 'class', 'teacher', 'class_type']]
+
+        return reordered_df
 
     @staticmethod
     def save(df:pd.core.frame.DataFrame,
@@ -106,31 +193,12 @@ class ScheduleScraper:
         Returns:
             None -- Prints success message"""
 
-        # should you just write over the file or append
-        # see write mode
         df.to_csv(path_or_buf=save_fp,
                   index=False,
-                  mode='a')
+                  mode='w')
 
         print("File has been saved to '{}'".format(save_fp))
 
-    def scrape_and_save(self, save_fp):
-
-        """Main method which scrapes the urls in url_dict, parses the page source,
-        and saves it to the specified save_fp"""
-
-        df_list = []
-
-        for key, value in self.url_dict:
-            soup = self.get_soup(value)
-            schedule_df = self.parse_soup(soup)
-            schedule_df['class_type'] = key
-            df_list += [schedule_df]
-
-        merged = pd.concat(df_list, axis = 1)
-        merged.sort_values(by='datetime', axis=1, inplace=True)
-        self.save(merged, save_fp=save_fp)
-
 if __name__ == "__main__":
     save_fp = 'saved/class_schedule.csv'
-    ScheduleScraper().scrape_and_save(save_fp=save_fp)
+    ss = ScheduleScraper()
